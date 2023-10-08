a886caf9d8e7570ef4539714bab54c9a0464a37a
조영준 <joyoungjun8590@hotmail.com>
2023-10-09 03:37:57 +0900

    마켓 1차 작업

--- src/pages/market/Market.tsx
+++ src/pages/market/Market.tsx
@@ -9,15 +9,36 @@ import {
   TabPanel,
 } from "@chakra-ui/react";
 
+import strip from '../../resource/strip.png'
+
 export default function Market() {
   return (
     <Box backgroundColor={"black"} pt={10}>
       <Container maxW={"1200px"}>
-        <Tabs>
+        <Tabs borderColor={'#202020'}>
           <TabList>
-            <Tab></Tab>
-            <Tab>Two</Tab>
-            <Tab>Three</Tab>
+            <Tab
+              p={'8px 28px 8px 28px'}
+              letterSpacing={'-1px'}
+               color={'gray'} _selected={{
+                color: "#9A8866",
+                backgroundImage: strip,
+                fontWeight: "bold",
+              }}>신규 판매 글</Tab>
+            <Tab
+              p={'8px 28px 8px 28px'}
+              letterSpacing={'-1px'} color={'gray'} _selected={{
+                color: "#9A8866",
+                backgroundImage: strip,
+                fontWeight: "bold",
+              }}>인기있는 아이템</Tab>
+            <Tab 
+              p={'8px 28px 8px 28px'}
+              letterSpacing={'-1px'} color={'gray'} _selected={{
+                color: "#9A8866",
+                backgroundImage: strip,
+                fontWeight: "bold",
+              }}>최근 판매된 아이템</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
