ba7292fc78e9488ca8e1c0634f3a5c41306b17fc
조영준 <joyoungjun8590@hotmail.com>
2023-10-10 00:27:41 +0900

    마켓 수정

--- src/components/market/NewRegItem.tsx
+++ src/components/market/NewRegItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, VStack } from "@chakra-ui/react";
 import { MarketListItem } from "./MarketListItem";
 import MarketItem from "./item/item";
 
@@ -7,37 +7,25 @@ import Stripe from "../../resource/texture/strip.png";
 export default function NewRegItem() {
   return (
     <>
-      <Box p={"32px 0 32px 0"}>
-        <Text
-          fontWeight={"bold"}
-          color={"#9a8866"}
-          letterSpacing={"-1px"}
-          fontSize={"18px"}
-        >
-          신규
-        </Text>
-        <Text fontSize={"14px"} color={"#AEAEB0"} pt={4} mb={"30px"}>
-          가장 최근에 등록된 아이템들을 보여드립니다. <br />
-        </Text>
+      <Box>
         <Flex
           padding='10px'
           w='100%'
           backgroundColor={"#171715"}
           marginBottom={"10px"}
-          borderRadius={"10px"}
           backgroundImage={Stripe}
         >
           <Box w='100px'>
-            <Text marginLeft={"5px"} borderRight={"1px solid white"}>
+            <Text fontSize={'14px'}  marginLeft={"5px"} borderRight={"1px solid white"}>
               이미지
             </Text>
           </Box>
           <Box>
-            <Text marginLeft={"30px"}>상품 정보</Text>
+            <Text fontSize={'14px'} marginLeft={"30px"}>상품 정보</Text>
           </Box>
         </Flex>
         <Box>
-          <Box>
+          <VStack spacing={3}>
             <MarketItem
               name='RGD-5'
               category='gun'
@@ -92,7 +80,7 @@ export default function NewRegItem() {
               sellerAvatar='https://mblogthumb-phinf.pstatic.net/MjAxODExMDZfMjk1/MDAxNTQxNDgzMjQ2Njkx.nfpggc9vN0l5-p8vjtc9ddl9JZXnqDZseeiBxSEIzeMg.Dv8HMLHaYQNVqobLTNJZ-GgcsF0uylmECBzzBN1ZfJgg.JPEG.kidarinusu/1541483245673.jpg?type=w800'
               key={1}
             />
-          </Box>
+          </VStack>
         </Box>
       </Box>
     </>
--- src/components/market/item/item.tsx
+++ src/components/market/item/item.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, VStack } from "@chakra-ui/react";
 
 interface MarketItemProps {
   name: string;
@@ -11,66 +11,17 @@ interface MarketItemProps {
 
 export default function MarketItem(props: MarketItemProps) {
   return (
-    <Link textDecoration={"none"}>
-      <Flex
-        position={"relative"}
-        backgroundColor={"#171715"}
-        padding={"10px"}
-        marginBottom={"10px"}
-        borderRadius={"10px"}
-        border={"0px solid #9A8866"}
-        boxShadow='0px 0px 0px 0px #9A8866'
-        _hover={{
-          border: "1px solid #9A8866",
-          boxShadow: "3px 3px 0px 0px #9A8866;",
-          transform: "translateX(-3px) translateY(-3px)",
-          transitionDuration: "0.5s",
-          transitionTimingFunction: "ease-in-out",
-        }}
-      >
-        <Image
-          src={props.image}
-          w={"100px"}
-          border={"2px solid #646464"}
-          borderRadius={"5px"}
-        />
-        <Flex
-          direction='column'
-          justifyContent={"center"}
-          flex={3}
-          padding={"10px"}
-          pl={"30px"}
-        >
-          <Text fontSize={"13px"} color={"rgba(255, 199, 86, 0.589)"}>
-            아이템
-          </Text>
-          <Text fontSize={"20px"}>{props.name}</Text>
-          <Text fontSize={"13px"} color={"#949494"}>
-            {props.category}
-          </Text>
-        </Flex>
-        <Flex
-          justifyContent={"center"}
-          direction={"column"}
-          flex={0.5}
-          padding={"10px"}
-        >
-          <Flex flex={1} alignItems={"center"}>
-            <Image
-              w='20px'
-              h='20px'
-              src={props.sellerAvatar}
-              borderRadius={"100%"}
-              mr='10px'
-            />
-            <Text fontSize={"13px"}>{props.seller}</Text>
-          </Flex>
-          <Text flex={1}>
-            {"$"}
-            {props.price} ~
-          </Text>
-        </Flex>
-      </Flex>
-    </Link>
+    <HStack h={'75px'} w={'100%'} bgColor={'#171715'} p={'8px 8px 8px 8px'} spacing={5}>
+      <Image 
+        src={props.image}
+        objectFit={'cover'}
+        w={'63px'}
+        border={'1px solid #9a8866'}
+      />
+      <VStack alignItems={'flex-start'} spacing={1}>
+        <Text fontSize={'11px'} color={'#AEAEB0'}>{props.category}</Text>
+        <Text fontSize={'14px'} fontWeight={'bold'} color={'#9a8866'}>{props.name}</Text>
+      </VStack>
+    </HStack>
   );
 }
